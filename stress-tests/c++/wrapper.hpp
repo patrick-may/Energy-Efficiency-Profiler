507e8100378962d248ce25afbf390966f7ad4859
patrick-may <pmay24@wooster.edu>
2023-04-22 16:20:55 -0400

    documentation added

--- stress-tests/c++/wrapper.hpp
+++ stress-tests/c++/wrapper.hpp
@@ -6,7 +6,6 @@
 #include <fstream>
 
 #define COWPROF_FILEPATH "data/intervals/c++/TimeLog-2023-04-22-16-02-22.819401.csv"
-using namespace std::chrono;
 
 class CowLog {
 
@@ -15,21 +14,24 @@ class CowLog {
         uint64_t begms;
         uint64_t endms;
 
+        // timestamp beginning of spot of interest
         CowLog(std::string fname){
             funcname = fname;
-            begms = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
+            begms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
         }
 
+        // timestamp end, append entire instance info
         void finish(){
-            endms = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
+            endms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
             std::string writeme = funcname + ',' + conv(begms) + ',' + conv(endms) +'\n';
             std::ofstream outfile;
             outfile.open(COWPROF_FILEPATH, std::ios::app);
             outfile << writeme;
-            outfile.clear();
+            outfile.close();
         }
 
     private:
+        // hack to transform millisecond since epoch to string of seconds since epoch
         std::string conv(uint64_t ms){
             std::string ms_conv = std::to_string(ms);
             int decloc = ms_conv.size() - 3;
